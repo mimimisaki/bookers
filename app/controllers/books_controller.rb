dbc1d283460023f58685d4d6ee00d9dfe75430ca
mimimisaki <mimimisaki2@gmail.com>
2020-04-29 23:20:41 +0900

    [Fix]提出1回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
-  		redirect_to book_path(@book.id)
+  		redirect_to book_path(@book.id), notice: 'Book was successfully created.'
   	else
   		@books = Book.all
   		render :index
@@ -26,15 +26,18 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to books_path
+  	redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
 
